9ed0ea102d747e114f26e2dccfb33d9c914ca0b9
Marcelo <mhsf15@hotmail.com>
2023-02-28 20:23:50 -0300

    adicionando conexao com o banco mysql

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,20 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
 
 router.get('/',(req,res,next) => {
-    res.status(200).send({
-        mensagem: "Usando Get Dentro da rota produtos"
+    mysql.getConnection((erros,conn) => {
+        conn.query("SELECT * FROM Produtos"),[],
+        (error,resultado,field) => {
+            conn.release();
+            if(error){
+                return res.status(500).send({
+                    erros: error,
+                    response:null
+                })
+            }
+            return res.status(200).send({
+                response:resultado
+            })
+
+        }
     })
+    
 })
 
 router.post('/',(req,res,next)=>{
-    const produto = {
-        nome: req.body.nome,
-        preco: req.body.preco
-    }
-    res.status(200).send({
-        mensagem: "Usando post dentro da rota produtos",
-        ProdutoCriado: produto
+    mysql.getConnection((error,conn) => {
+        debugger
+        console.log(conn)
+        conn.query(
+            'INSERT INTO Produtos (nome,preco) VALUES (?,?)',
+            [req.body.nome,req.body.preco],
+            (error,resultado,field) => {
+                conn.release();
+
+                if(error){
+                    return res.status(500).send({
+                        erros:error,
+                        response: null,
+                    })
+                }
+
+                return res.status(201).send({
+                    mensagem: "Produto Inserido com sucesso",
+                    Id_produto : resultado.insertId
+                })
+            })
+        
     })
 })
 
